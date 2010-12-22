b1823db57427a0840f075a7361b41d9c196fb3b5
Pasha Puzikov <heydiplo@gmail.com>
2010-12-23 01:57:27 +0300

    bugfixes

--- lib/active_record/magic_numbers.rb
+++ lib/active_record/magic_numbers.rb
@@ -9,14 +9,14 @@ module ActiveRecord
         module ClassMethods
             
             def enum_attribute(name, values, options={})
-                magic_number_attribute(name, options.extend({
+                magic_number_attribute(name, options.merge({
                     :values => values,
                     :type => :enum
                 }))
             end
 
             def bitfield_attribute(name, values, options={})
-                magic_number_attribute(name, options.extend({
+                magic_number_attribute(name, options.merge({
                     :values => values,
                     :type => :bitfield
                 }))
@@ -27,7 +27,7 @@ module ActiveRecord
             end
 
             def magic_number_for(name, value)
-                options = self.class.magic_number_options(name)
+                options = self.magic_number_options(name)
                 values = options[:values]
 
                 value = value.is_a?(Array) ? value.map(&:to_s) : value.to_s
@@ -35,15 +35,15 @@ module ActiveRecord
                 if options[:type] == :bitfield
                     (values & value).map { |v| 2**values.index(v) }.sum
                 else
-                    values[value]
+                    values.index(value)
                 end
             end
 
-            protected
-
-            def magic_number_options(read)
-                read_inheritable_attribute(:magic_number_attributes) || {}
+            def magic_number_options(name)
+                read_inheritable_attribute(:magic_number_attributes)[name]
             end
+
+            protected
 
             def magic_number_attribute(name, options)
                 options.assert_valid_keys(:values, :type, :default) 
@@ -55,9 +55,9 @@ module ActiveRecord
                 write_inheritable_attribute(:magic_number_attributes, magic_number_attributes)
 
                 class_eval <<-EOE
-                    def #{name}; magic_numbers_read(:#{name}); end
-                    def #{name}=(new_value); magic_numbers_write(:#{name}, new_value); end
-                    def self.#{name}_values; magic_numbers_values(:#{name}); end
+                    def #{name}; magic_number_read(:#{name}); end
+                    def #{name}=(new_value); magic_number_write(:#{name}, new_value); end
+                    def self.#{name}_values; magic_number_values(:#{name}); end
                 EOE
             end
         end
@@ -73,11 +73,11 @@ module ActiveRecord
                     result = values.reject { |r| ((mask_value || 0) & 2**values.index(r)).zero? }
                     (result.empty? && options[:default]) ? options[:default] : result
                 else
-                    values[mask_value] || options[:default]
+                  (mask_value && values[mask_value]) || options[:default]
                 end
             end
 
-            def magic_numbers_write(name, new_value)
+            def magic_number_write(name, new_value)
                 self["#{ name }_mask"] = self.class.magic_number_for(name, new_value)
             end
 
